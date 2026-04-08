c83152ceb2c215c92fba988305711f0045ae6608
Jacqode <45409429+Jacqode@users.noreply.github.com>
2026-04-08 19:23:06 +0200

    nyt reminder forsøg

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
+// Sørger for at SW aktiveres med det samme
 self.addEventListener("install", () => self.skipWaiting());
+
+// Sørger for at SW tager kontrol over alle klienter
 self.addEventListener("activate", () => self.clients.claim());
+
+// Åbn Flow Focus i samme vindue hvis det allerede kører
 self.addEventListener("notificationclick", event => {
   event.notification.close();
 
