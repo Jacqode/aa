7e6d0638398cd7de7429459c8b7677b4fecd6810
Jacqode <45409429+Jacqode@users.noreply.github.com>
2026-04-08 19:18:28 +0200

    åbner ff i allerede åben ff tab hvis åben ellers åbnes ny

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,20 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
+
+      // Hvis Flow Focus allerede er åbent → fokusér det
+      for (const client of clientList) {
+        if (client.url.includes("/aa") && "focus" in client) {
+          return client.focus();
+        }
+      }
+
+      // Ellers → åbn et nyt vindue
+      return clients.openWindow(event.notification.data.url);
+    })
+  );
+});
