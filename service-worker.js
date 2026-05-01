d60a51a2a8beba95879916179ed6b1d23cc0c245
Jacqode <45409429+Jacqode@users.noreply.github.com>
2026-05-01 16:45:34 +0100

    212

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,13 @@ self.addEventListener("activate", () => self.clients.claim());
 self.addEventListener("notificationclick", event => {
   event.notification.close();
   event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true })
+    clients.matchAll({ type:"window", includeUncontrolled:true })
       .then(list => {
-        for (const client of list) {
-          if (client.url.includes("/aa") && "focus" in client) {
-            return client.focus();
-          }
+        for (const c of list) {
+          if (c.url.includes("/aa") && "focus" in c) return c.focus();
         }
         return clients.openWindow(event.notification.data.url);
       })
   );
 });
+
